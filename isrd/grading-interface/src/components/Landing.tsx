2981399c80e0ef0840aa5c54cffe6fb68d8d0866
Bowen Leng <bowenleng88@gmail.com>
2023-06-21 09:54:30 -0700

    no panic when data length is 0

--- isrd/grading-interface/src/components/Landing.tsx
+++ isrd/grading-interface/src/components/Landing.tsx
@@ -50,10 +50,8 @@ const UrlReaderComponent = () => {
         setSelectedTag(diagnosis_rid);
         console.log("choose:", diagnosis_rid);
         if (diagnosis_rid !== "empty") {
-            const data_url = 'http://localhost:8080/data';
-            // const data_url = `https://${host}/ermrest/catalog/eye-ai/attribute/Subject_Dataset:=eye-ai:Subject_Dataset/Dataset=${dataset_rid}/Subject:=left(Subject)=(eye-ai:Subject:RID)/Observation:=left(RID)=(eye-ai:Observation:Subject)/Image:=left(RID)=(eye-ai:Image:Observation)/Diagnosis:=left(RID)=(eye-ai:Diagnosis:Image)/Diagnosis_Tag=${diagnosis_rid}/Image_RID:=Image:RID,Image:URL,Image:Filename,Image:Length,Image:Image,Image:Image_Quality_Vocab,Diagnosis:Cup%2FDisk_Ratio,Diagnosis_RID:=Diagnosis:RID,Diagnosis_Image_RID:=Diagnosis:Image`
-            // const data_url = `https://${host}/ermrest/catalog/eye-ai/attribute/Subject_Dataset:=eye-ai:Subject_Dataset/Dataset=${dataset_rid}/Subject:=left(Subject)=(eye-ai:Subject:RID)/Observation:=left(RID)=(eye-ai:Observation:Subject)/Image:=left(RID)=(eye-ai:Image:Observation)/Diagnosis:=left(RID)=(eye-ai:Diagnosis:Image)/Diagnosis_Tag=${diagnosis_rid}/Image_RID:=Image:RID,Image:URL,Image:Filename,Image:Length,Image:Image,Image:Image_Quality_Vocab,Diagnosis:Cup%2FDisk_Ratio,Diagnosis_RID:=Diagnosis:RID,Diagnosis_Image_RID:=Diagnosis:Image`
-            // const data_url = `https://${host}/ermrest/catalog/eye-ai/attribute/Subject_Dataset:=eye-ai:Subject_Dataset/Dataset=${dataset_rid}/Subject:=left(Subject)=(eye-ai:Subject:RID)/Observation:=left(RID)=(eye-ai:Observation:Subject)/Image:=left(RID)=(eye-ai:Image:Observation)/Diagnosis:=left(RID)=(eye-ai:Diagnosis:Image)/Diagnosis_Tag=${diagnosis_rid}/Image_RID:=Image:RID,Image:URL,Image:Filename,Image:Length,Image:Image,Image:Image_Quality_Vocab,Diagnosis:Cup%2FDisk_Ratio,Diagnosis_RID:=Diagnosis:RID,Diagnosis_Image_RID:=Diagnosis:Image`;
+            // const data_url = 'http://localhost:8080/data';
+            const data_url = `https://${host}/ermrest/catalog/eye-ai/attribute/Subject_Dataset:=eye-ai:Subject_Dataset/Dataset=${dataset_rid}/Subject:=left(Subject)=(eye-ai:Subject:RID)/Observation:=left(RID)=(eye-ai:Observation:Subject)/Image:=left(RID)=(eye-ai:Image:Observation)/Diagnosis:=left(RID)=(eye-ai:Diagnosis:Image)/Diagnosis_Tag=${diagnosis_rid}/Image_RID:=Image:RID,Image:URL,Image:Filename,Image:Length,Image:Image,Image:Image_Quality_Vocab,Diagnosis:Cup%2FDisk_Ratio,Diagnosis_RID:=Diagnosis:RID,Diagnosis_Image_RID:=Diagnosis:Image`;
             console.log("generate url:", data_url);
 
             const fetchData = async () => {
@@ -120,14 +118,14 @@ const UrlReaderComponent = () => {
 
                 {/* {jsonData && <pre>{JSON.stringify(jsonData, null, 2)}</pre>} */}
 
-                {jsonData && jsonData.length > 0 && (
+                {/* {jsonData && jsonData.length > 0 && (
                     <button className='chaise-btn chaise-btn-primary' onClick={saveData}>Quit</button>
-                )}
+                )} */}
 
             </div>
-            {/* {jsonData === null && (
+            {jsonData && jsonData.length === 0 && (
                 <p> No available data. Please go back.</p>
-            )} */}
+            )}
 
             {jsonData && jsonData.length > 0 && (
                 <>
